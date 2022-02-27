53ef02533c6c5f1b063e094a5954ac96acb49d4e
m-kuoch <m.kuoch1@gmail.com>
2022-02-27 18:19:08 -0500

    change player collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20f;
+    private float speed = 400f;
     private float turnSpeed = 30f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +19,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         // Move player forwards based on input
-        transform.Translate(Vector2.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector2.up * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector2.right * Time.deltaTime * speed * horizontalInput);
+        //transform.Translate(Vector2.up * Time.deltaTime * speed * forwardInput);
         // Rotate vehicle baesd on input
         //transform.Rotate(Vector2, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // GetComponent<Rigidbody2D>().velocity = Vector2.right * Time.deltaTime * speed * horizontalInput;
+        // GetComponent<Rigidbody2D>().velocity = Vector2.up * Time.deltaTime * speed * verticalInput;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Time.deltaTime * speed * horizontalInput,
+            Time.deltaTime * speed * verticalInput);
+        
+        Debug.Log(horizontalInput);
+        
+        //GetComponent<Rigidbody2D>().AddForce(Vector2.right * Time.deltaTime * speed * horizontalInput, ForceMode2D.Impulse);
+        //GetComponent<Rigidbody2D>().AddForce(Vector2.up * Time.deltaTime * speed * verticalInput, ForceMode2D.Impulse);
     }
 }

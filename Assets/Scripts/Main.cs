276382ef76b58a68ed753a49a9a64b72521151cc
Enrique <enriquec@mit.edu>
2022-03-01 15:36:34 -0500

    Reset position of ball and player on score

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject ball;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +33,11 @@ public class Main : MonoBehaviour
     void ResetGame(string goalName)
     {
         Debug.Log("Yoyoyo the event callback worked! " + goalName);
+        player.transform.position = new Vector2(10, 0);
+
+        // Reset ball position and velocity
+        ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        ball.transform.position = new Vector2(0, 0);
         return;
     }
 }
